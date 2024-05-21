023647767184afe4159d537fd5ea400cda7e7fc3
yahya <yahyaoulha0@gmail.com>
2024-05-21 16:52:34 +0100

    worked

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+""" cashing system """
+
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    basic LRU caching class
+    """
+    CacheLog = []
+
+    def put(self, key, item):
+        """
+        Add an item in the cache
+        """
+        if key is not None or item is not None:
+            if key in self.cache_data:
+                self.CacheLog.remove(key)
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                LRUKey = self.CacheLog.pop(-1)
+                del self.cache_data[LRUKey]
+                print("DISCARD:", LRUKey)
+
+            self.CacheLog.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Get an item by key
+        """
+        if key is not None and key in self.cache_data:
+            self.CacheLog.remove(key)
+            self.CacheLog.append(key)
+            return self.cache_data[key]
