6d56e7665da303dbbb04adb0b8f155f2efd7f412
yahya <yahyaoulha0@gmail.com>
2024-05-21 16:47:00 +0100

    worked

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -10,42 +10,28 @@ class LRUCache(BaseCaching):
     """
     basic LRU caching class
     """
-    CacheLog = {}
-
-    def UpdateLog(self, key, value):
-        if key not in self.CacheLog:
-            self.CacheLog[key] = 0
-
-        for k in self.CacheLog.keys():
-            if self.cache_data[k] == value:
-                continue
-            self.CacheLog[k] += 1
-
-    def GetLRU(self):
-        LRUKey = max(self.CacheLog, key=lambda k: self.CacheLog[k])
-        self.CacheLog[LRUKey] = 0
-        return LRUKey
+    CacheLog = []
 
     def put(self, key, item):
         """
         Add an item in the cache
         """
         if key is not None or item is not None:
-            self.cache_data[key] = item
-            self.UpdateLog(key, item)
-
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                LRUKey = self.GetLRU()
-                print("DISCARD: {}".format(LRUKey))
+            if key in self.cache_data:
+                self.CacheLog.remove(key)
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                LRUKey = self.CacheLog.pop(0)
                 del self.cache_data[LRUKey]
-                del self.CacheLog[LRUKey]
+                print("DISCARD:", LRUKey)
+
+            self.CacheLog.append(key)
+            self.cache_data[key] = item
 
     def get(self, key):
         """
         Get an item by key
         """
-        if key is not None:
-            try:
-                return self.cache_data[key]
-            except KeyError:
-                pass
+        if key is not None and key in self.cache_data:
+            self.CacheLog.remove(key)
+            self.CacheLog.append(key)
+            return self.cache_data[key]
