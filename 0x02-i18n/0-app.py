04148a1ed32e1b06690f6801d110125b3d814776
yahya <yahyaoulha0@gmail.com>
2024-05-28 12:16:29 +0100

    done

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -3,9 +3,21 @@
 """simple flask webapp"""
 
 from flask import Flask, render_template
+from flask_babel import Babel
 
 
 myapp = Flask(__name__)
+babel = Babel(myapp)
+
+
+class Config(object):
+    """lang config class"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+myapp.config.from_object(Config)
 
 
 @myapp.route("/")
